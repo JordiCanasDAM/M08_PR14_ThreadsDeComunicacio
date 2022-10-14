087a09c609eb6617ff78d0ed4d6e695a070a44dc
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2022-10-14 18:42:46 +0200

    E/AndroidRuntime: FATAL EXCEPTION: main     Process: com.example.m08_pr14_threadsdecomunicacio, PID: 3187     android.os.NetworkOnMainThreadException

--- app/src/main/java/com/example/m08_pr14_threadsdecomunicacio/MainActivity.java
+++ app/src/main/java/com/example/m08_pr14_threadsdecomunicacio/MainActivity.java
@@ -2,7 +2,23 @@ package com.example.m08_pr14_threadsdecomunicacio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +26,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override public void onClick(View view) {
+                getDataFromUrl("https://api.myip.com");
+            }
+        });
+    }
+
+    private String getDataFromUrl(String demoIdUrl) {
+
+        String result = null;
+        int resCode;
+        InputStream in;
+        String error = "";
+        try {
+            URL url = new URL(demoIdUrl);
+            URLConnection urlConn = url.openConnection();
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                in.close();
+                result = sb.toString();
+            } else {
+                error += resCode;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
