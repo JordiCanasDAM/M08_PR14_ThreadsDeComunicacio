2969c81251e5c791d94f063bb6b82f0320bca01d
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2022-10-17 17:03:38 +0200

    ListView ViewRootImpl$CalledFromWrongThreadException.

--- app/src/main/java/com/example/m08_pr14_threadsdecomunicacio/MainActivity.java
+++ app/src/main/java/com/example/m08_pr14_threadsdecomunicacio/MainActivity.java
@@ -12,7 +12,11 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -25,6 +29,7 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -37,8 +42,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inicialitzem model
+        ArrayList<String> lines = new ArrayList<String>();
+        ArrayAdapter<String> adapter;
+
+        // Inicialitzem l'ArrayAdapter amb el layout pertinent
+        adapter = new ArrayAdapter<String>( this, R.layout.activity_list_item, lines )
+        {
+            @Override
+            public View getView(int pos, View convertView, ViewGroup container)
+            {
+                if( convertView==null ) {
+                    convertView = getLayoutInflater().inflate(R.layout.activity_list_item, container, false);
+                }
+                ((TextView) convertView.findViewById(R.id.textView)).setText(getItem(pos));
+                return convertView;
+            }
+
+        };
+
+        // busquem la ListView i li endollem el ArrayAdapter
+        ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
         Button button = findViewById(R.id.button);
-        TextView text = findViewById(R.id.textView);
+        ListView text = findViewById(R.id.listView);
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override public void onClick(View view) {
@@ -50,9 +78,11 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         String data = getDataFromUrl("https://api.myip.com");
                         Log.i("INFO:", data);
+                        lines.add(data);
+                        adapter.notifyDataSetChanged();
                         handler.post(new Runnable() {
                             @Override public void run() {
-                                text.setText(text.getText()+""+data);
+
                             }
                         });
                     }
